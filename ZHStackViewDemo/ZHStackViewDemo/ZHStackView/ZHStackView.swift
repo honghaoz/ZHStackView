e7172c8c5b29c521be9b04f542cc3ec0565daf04
Honghao Zhang <zhh358@gmail.com>
2014-10-17 12:25:47 -0400

    Initial commit

---
+++ ZHStackViewDemo/ZHStackViewDemo/ZHStackView/ZHStackView.swift
@@ -0,0 +1,22 @@
+//
+//  ZHStackView.swift
+//  ZHStackViewDemo
+//
+//  Created by Honghao Zhang on 2014-10-17.
+//  Copyright (c) 2014 HonghaoZ. All rights reserved.
+//
+
+import UIKit
+
+class ZHStackView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect)
+    {
+        // Drawing code
+    }
+    */
+
+}
